74539b8b00edf49a2c239d8ea8892fb16dc1bb46
Kaiyuan Wang <wangkaiyuanzz@gmail.com>
2018-05-17 13:16:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ sudo apt-get install emacs
 rm $HOME/opt/*.tar.gz
 ssh-keygen -t rsa -C "wangkaiyuanzz@gmail.com"
 
-sudo apt-get install xvfb
-sudo apt-get install xorg
-sudo apt-get install gtk2.0
+# For Reks
+# sudo apt-get install xvfb
+# sudo apt-get install xorg
+# sudo apt-get install gtk2.0
